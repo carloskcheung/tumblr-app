c7903821e88d7f056f923399e292d5c5d81a3f16
Carlos Cheung <cc653@georgetown.edu>
2015-05-30 14:03:34 -0700

    Initial Commit

---
+++ Tumblr-AppTests/Tumblr_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Tumblr_AppTests.swift
+//  Tumblr-AppTests
+//
+//  Created by Carlos Cheung on 5/30/15.
+//  Copyright (c) 2015 carloscheung. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Tumblr_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
